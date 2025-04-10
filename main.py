0e0f4ab194cc8022782141a18c7c8ad52e7720b0
sgromme <wills1377@gmail.com>
2025-04-10 14:46:31 -0400

    main.py  new endpoint.

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = FastAPI()
 def top():
     return "top here"
 
+@app.get("/echo/{thing}") # command line parameter
+def echo(thing: str):
+    return f"echoing the man {thing}"
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", reload=True)
